5637c6c94b749791ff2d194f008c31f3adbcd8f4
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-07-05 19:33:30 +0530

    layout for myAsyncTask Runner

--- app/src/main/java/com/appbusters/robinkamboj/backgoundcomponents/view/fragments/TwoFragment.java
+++ app/src/main/java/com/appbusters/robinkamboj/backgoundcomponents/view/fragments/TwoFragment.java
@@ -1,6 +1,8 @@
 package com.appbusters.robinkamboj.backgoundcomponents.view.fragments;
 
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -28,4 +30,28 @@ public class TwoFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_two, container, false);
     }
 
+    private class MyAsyncTaskRunner extends AsyncTask<String, String, String>{
+
+        ProgressDialog dialog;
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+        }
+    }
 }
